251c1b1fa1a3013ffb71f85e559e609eccbfe6b6
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-12 16:31:20 -0500

    spotify is almost done, just need to set the default song when process.agrv[3] is blank

--- liri.js
+++ liri.js
@@ -17,12 +17,38 @@ spotify
 .search({ type: 'track', query: process.argv[3] })
   .then(function(data) {
     const drilled = data.tracks.items[0]; 
-    console.log(drilled.album.name);
-    
+    console.log("Artist:  " + drilled.album.artists[0].name);
+    console.log("-------------------------------------");
+    console.log("Song Name:  " + drilled.name);
+    console.log("-------------------------------------");
+    console.log("Preview Link:  " + drilled.preview_url);
+    console.log("-------------------------------------");
+    console.log("Album Name:  " + drilled.album.name);
+
 
   })
   .catch(function(err) {
     console.error('Error occurred: ' + err); 
   });
 }
-      
+       
+if ( process.argv[3] === ""){
+
+  spotify
+  .search({ type: 'track', query: "The Sign" })
+    .then(function(data) {
+      const drilled = data.tracks.items[0]; 
+      console.log("Artist:  " + drilled.album.artists[0].name);
+      console.log("-------------------------------------");
+      console.log("Song Name:  " + drilled.name);
+      console.log("-------------------------------------");
+      console.log("Preview Link:  " + drilled.preview_url);
+      console.log("-------------------------------------");
+      console.log("Album Name:  " + drilled.album.name);
+  
+  
+    })
+    .catch(function(err) {
+      console.error('Error occurred: ' + err); 
+    });
+  }
